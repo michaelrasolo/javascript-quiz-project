6bdb3480138f6ddcb12eb5e1a5386d62689db7a6
“Samara <samara.limads@gmail.com>
2023-12-11 16:55:33 +0100

    question class

--- src/question.js
+++ src/question.js
@@ -1,7 +1,29 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    // console.log(this.choices);
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      let temp = this.choices[i];
+      this.choices[i] = this.choices[j];
+      this.choices[j] = temp;
+    }
+    // console.log(this.choices);
+    return this.choices;
+  }
 }
+
+const question1 = new Question(
+  "text",
+  ["choice1", "choice2", "choice3"],
+  "answer",
+  5
+);
+
+question1.shuffleChoices();
